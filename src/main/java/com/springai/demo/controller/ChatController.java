6e92a10ce767bc00f0d2cd8587c8a6fe2bc67bca
ayushi-chopra <ayushi94ac@gmail.com>
2026-02-01 00:03:57 +0530

    setup with ollama

---
+++ src/main/java/com/springai/demo/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.springai.demo.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClient){
+        this.chatClient=chatClient.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message){
+        return this.chatClient.prompt(message).call().content();
+    }
+}
